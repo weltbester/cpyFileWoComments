47c55b635b199841b92dde9cbc4126ca77db4e72
weltbester <georgpohl@gmx.de>
2020-08-14 13:46:33 +0200

    finished program for two-argument use

--- cpyFileWoComments.c
+++ cpyFileWoComments.c
@@ -1,10 +1,21 @@
 /** 
- * Copyright July 2020: Georg Pohl, 70174 Stuttgart
+ * Copyright August 2020: Georg Pohl, 70174 Stuttgart
  * File: cpyFileWoComments.c
- * --------
- * Copies a file with comments into another file without
- * copying annoying comments. Comments must be surrounded
- * by two equal delimiters.
+ * -------------------------
+ * Copies a (skeleton) file with comments into another
+ * (working) file without copying annoying comments.
+ * Comments must be surrounded by two self-chosen, but
+ * equal, single-character delimiters,
+ *
+ * If delimiters are unequal, you have to provide
+ * three arguments, otherwise only two.
+ * 
+ * Two examples:
+ * #comment# =>  ./cpyFileWoComments 'source filename' '#'
+ *
+ * not possisble a.t.m.
+ * <comment> =>  ./cpyFileWoComments 'source filename' '<' '>'
+ * 
  */
 
 #include <stdio.h>
@@ -23,39 +34,38 @@
 int main(int argc, const char **argv) {
   FILE *infile, *outfile;
 
+  // Check input arguments
   if ( argc < 2) {
-    printf("Usage: ./cpyFileWoComments <source filename> '<startDelimiter>', '<endDelimiter>' (optional)\n");
+    printf("Usage: ./cpyFileWoComments <source filename> 'startDelimiter', 'endDelimiter' (2nd optional, if the same)\n");
     return EXIT_FAILURE;
   }
   if ( (infile = fopen(argv[1], "r")) == NULL ) {
-    fprintf(stderr, "Fehler beim Oeffnen der Originaldatei!\n");
+    fprintf(stderr, "Could not open source file!\n");
     return EXIT_FAILURE;
   }
-
-  char outFileName[255];
-  strcpy(outFileName, argv[1]);
+  // Create output filename and file
+  char outFileName[MAX];
+  strncpy(outFileName, argv[1], MAX-4);
   strcat(outFileName, "_cpy");
   if ( (outfile = fopen(outFileName, "w")) == NULL ) {
-    fprintf(stderr, "Fehler beim Oeffnen der Kopie!\n");
+    fprintf(stderr, "Could not open copy file!\n");
     return EXIT_FAILURE;
   }
- 
+  // Copy only relevant Bytes
   int ch;
-  int delStart = '%';
-  //strcpy(delStart, argv[2]);
-  int delEnd = '%';
-  //strcpy(delEnd, argv[3]);
+  int del = argv[2][0];
   while ( (ch = fgetc(infile)) != EOF ) {
-    if (ch == delStart) {
+    if (ch == del) {
       do {
 	ch = fgetc(infile);
-      } while (ch != delEnd);
+      } while (ch != del);
       
     }
     else {
       fputc(ch, outfile);
     }
   }
+  // Close all opened files
   fclose(infile);
   fclose(outfile);
 
